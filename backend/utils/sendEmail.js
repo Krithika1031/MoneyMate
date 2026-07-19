c0d52acb26800121110b8ecdf815cc2ac2914543
Krithika1031 <krithikabhat2006@gmail.com>
2026-07-19 22:14:35 +0530

    Checking SMTP settings

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -6,14 +6,14 @@ const sendEmail = async (to, subject, html) => {
 console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
     const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
-
     await transporter.sendMail({
       from: `"MoneyMate" <${process.env.EMAIL_USER}>`,
       to,
