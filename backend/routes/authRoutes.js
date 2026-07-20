92d5539a3505b1ae6daa09bf1d65c7cac3720693
Krithika1031 <krithikabhat2006@gmail.com>
2026-07-21 00:31:33 +0530

    Authroutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,4 @@
 import express from "express";
-
 import {
   registerUser,
   loginUser,
@@ -7,16 +6,12 @@ import {
 } from "../controllers/authController.js";
 
 import authMiddleware from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 
-// Register
 router.post("/register", registerUser);
-
-
-// Login
 router.post("/login", loginUser);
 
-// Change Password
 router.put(
   "/change-password",
   authMiddleware,
